83b2283e7f6380691f104f64a74748d12e33849f
xa <xa>
2020-04-30 11:55:51 +0200

    add licence

--- client/react/src/js/component/registration/authenticator/index.js
+++ client/react/src/js/component/registration/authenticator/index.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
 import InputForm from "../../forms/InputForm";
 
@@ -23,23 +39,20 @@ class DeviceRegistration extends React.Component {
         const {message,registrationStatus,email} = this.props;
 
         return (
-
-                <form className="measure" key="registration_authenticator_form">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                        <legend className="f4 fw6 ph0 mh0">Register Authenticator</legend>
-                        <div className="mt3" key="ppp">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                            <InputForm name="user_email" value={email} type="email" onChangeText={this.handleInputChange}/>
-                        </div>
-                    </fieldset>
-                    <div>
-                        <button name="register" value="Register" onClick={this.onRegisterDeviceClick} className="b ph3 pv2 input-reset ba  grow pointer f6 dib">Register</button>
+            <form className="measure" key="registration_authenticator_form">
+                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                    <legend className="f4 fw6 ph0 mh0">Register Authenticator</legend>
+                    <div className="mt3" key="ppp">
+                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                        <InputForm name="user_email" value={email} type="email" onChangeText={this.handleInputChange}/>
                     </div>
-                    <div className={`mv3 ${registrationStatus ? 'red':'green'}`}> {message}</div>
-                    </form>
-
-
-                )
+                </fieldset>
+                <div>
+                    <button name="register" value="Register" onClick={this.onRegisterDeviceClick} className="b ph3 pv2 input-reset ba  grow pointer f6 dib">Register</button>
+                </div>
+                <div className={`mv3 ${registrationStatus ? 'red':'green'}`}> {message}</div>
+            </form>
+        )
     }
 }
 
