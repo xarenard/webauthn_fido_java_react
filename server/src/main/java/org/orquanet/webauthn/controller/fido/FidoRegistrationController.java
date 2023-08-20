9a5e8acb3e8e1fe217d3d488a5da637be97c2553
yush <yushuhao_code@163.com>
2023-08-20 12:50:19 +0800

    Update FidoRegistrationController.java

--- server/src/main/java/org/orquanet/webauthn/controller/fido/FidoRegistrationController.java
+++ server/src/main/java/org/orquanet/webauthn/controller/fido/FidoRegistrationController.java
@@ -39,7 +39,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.server.PathParam;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -131,7 +130,7 @@ public class FidoRegistrationController  extends FidoController {
     @CrossOrigin(origins = "${webauthn.origins.allowed}", allowCredentials = "true", methods = {RequestMethod.POST})
     @PostMapping(path = "/registration/final/{id}", consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public void registerFinal(@PathParam ("id") String id,@RequestBody AuthenticatorAttestationResponseWrapper authenticatorAttestationResponseWrapper, HttpServletRequest request) throws Exception {
+    public void registerFinal(@PathVariable ("id") String id,@RequestBody AuthenticatorAttestationResponseWrapper authenticatorAttestationResponseWrapper, HttpServletRequest request) throws Exception {
 
         HttpSession session = request.getSession();
         WebauthnSession webauthnSession = (WebauthnSession) session.getAttribute(REGISTRATION_SESSION_NAME);
@@ -139,7 +138,7 @@ public class FidoRegistrationController  extends FidoController {
 
         String fidoId = webauthnSession.getFidoUser().getFidoId();
 
-        if(fidoId == null || fidoId.equals(id)){
+        if(fidoId == null || !fidoId.equals(id)){
             throw new RegistrationException("Invalid Fido User Id");
         }
 
